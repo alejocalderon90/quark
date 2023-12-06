f3c874e79de952bb3c7a36d5573546abf478f617
alejocalderon90 <santiago.alejo.calderon@gmail.com>
2023-12-06 20:43:05 -0300

    se crea galeria, fix navbar y se achican titulos

---
+++ js/lightBox.js
@@ -0,0 +1,30 @@
+function cambiarImagen1(nuevaImagen1) {
+    let mainImage1 = document.getElementById("main1")
+    mainImage1.src = nuevaImagen1
+}
+
+function cambiarImagen2(nuevaImagen2) {
+    let mainImag2 = document.getElementById("main2")
+    mainImag2.src = nuevaImagen2
+}
+
+function cambiarImagen3(nuevaImagen3) {
+    let mainImage3 = document.getElementById("main3")
+    mainImage3.src = nuevaImagen3
+}
+
+
+function cambiarImagen4(nuevaImagen4) {
+    let mainImage4 = document.getElementById("main4")
+    mainImage4.src = nuevaImagen4
+}
+
+function cambiarImagen5(nuevaImagen5) {
+    let mainImage5 = document.getElementById("main5")
+    mainImage5.src = nuevaImagen5
+}
+
+function cambiarImagen6(nuevaImagen6) {
+    let mainImage6 = document.getElementById("main6")
+    mainImage6.src = nuevaImagen6
+}
